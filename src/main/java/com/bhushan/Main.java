fd095ead05d84a146f4e8fc82232300a7b857c8b
Anant Bhushan <42645547+anant0036@users.noreply.github.com>
2024-05-07 16:43:43 +0530

    Code changes

--- src/main/java/com/bhushan/Main.java
+++ src/main/java/com/bhushan/Main.java
@@ -2,6 +2,6 @@ package com.bhushan;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Welcome");
     }
 }
